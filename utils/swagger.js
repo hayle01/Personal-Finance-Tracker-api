842927495bbf57208fdc1cd7cb81925473e88281
hayle01 <mabdirahim832@gmail.com>
2025-08-30 14:37:40 +0300

    Updated the server URL's

--- utils/swagger.js
+++ utils/swagger.js
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://personal-finance-tracker-api-tcxh.onrender.com'
+        url: process.env.SWAGGER_BASE_URL || 'http://localhost:3000'
       }
     ],
     components: {
