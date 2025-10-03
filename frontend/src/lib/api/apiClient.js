1205036ffcb847b421cacdb23dfdbe7a68222d6f
hayle01 <mabdirahim832@gmail.com>
2025-10-04 00:40:43 +0300

    Production URL

--- frontend/src/lib/api/apiClient.js
+++ frontend/src/lib/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import useAuthStore from '../Store/authStore';
 
-const API_URL = 'http://localhost:3000/api'; 
+const API_URL = 'https://personal-finance-tracker-api-tcxh.onrender.com/'; 
 
 const api = axios.create({
     baseURL: API_URL,
